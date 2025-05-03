c4ba875ef7dd26ded3cd04bd3e5d4b9af80ada37
VenCz <viniciuscesarp3@gmail.com>
2025-05-03 18:29:54 -0300

    Primeiro commit

---
+++ src/main/java/br/com/carbonNow/carbonNowAPI/CarbonNowApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.carbonNow.carbonNowAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarbonNowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarbonNowApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/carbonNow/carbonNowAPI/CarbonNowApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.carbonNow.carbonNowAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarbonNowApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
