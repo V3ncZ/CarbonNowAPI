0675495557d712390366eff2af9d928d6463f65d
VenCz <viniciuscesarp3@gmail.com>
2025-05-11 10:20:36 -0300

    feat(ApplicationExcpetionHandler): Implementar Excpetions Handler

---
+++ src/main/java/br/com/carbonNow/carbonNowAPI/advice/ApplicationExcpetionHandler.java
@@ -0,0 +1,38 @@
+package br.com.carbonNow.carbonNowAPI.advice;
+
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestControllerAdvice
+public class ApplicationExcpetionHandler {
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public Map<String, String> handle(MethodArgumentNotValidException erro) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrors = erro.getBindingResult().getFieldErrors();
+
+        for (FieldError error : fieldErrors) {
+            errors.put(error.getField(), error.getDefaultMessage());
+        }
+
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public Map<String, String> handle(DataIntegrityViolationException erro) {
+        Map<String, String> errors = new HashMap<>();
+        errors.put("error", "Conflito de dados: " + erro.getMostSpecificCause().getMessage());
+        return errors;
+    }
+}
